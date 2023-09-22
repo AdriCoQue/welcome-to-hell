5463c1a36def47bf2ec325f44a4bd72591089e38
ChanequeSlayer <82856592+ChanequeSlayer@users.noreply.github.com>
2023-09-22 12:19:32 -0600

    Archivo Base Principal
    Basarse en esto para lo siguiente.

---
+++ config.h
@@ -0,0 +1,7 @@
+const char* ssid     = "R ITCHII Wi-Fi";
+const char* password = "";
+const char* hostname = "riego";
+
+IPAddress ip(10, 8, 9, 130);
+IPAddress gateway(8, 8, 8, 8);
+IPAddress subnet(255, 255, 255, 0);
